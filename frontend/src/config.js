43a9ccecab7e30e956b6e85f3bee65acbcfce1d9
LoukikNaik <loukiknaik@gmail.com>
2025-05-17 10:39:21 -0700

    Railway app url for backend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  backendUrl: 'https://33b0-2601-646-0-36e0-4835-1d37-43fa-d2d.ngrok-free.app'
+  backendUrl: 'https://youingest-production.up.railway.app'
 };
 
 export default config; 
